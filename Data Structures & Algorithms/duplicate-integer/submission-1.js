d60886f83f195bd9b1ed12f6d0bf2915319b1d0b
ankur_singhal <62597979+AnkurSinghal05@users.noreply.github.com>
2026-06-17 08:01:16 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums) {
+        const map = {}
+        for (let i = 0; i< nums.length; i++){
+            if(map[nums[i]]){
+                console.log('in')
+                return true
+            }else{
+                map[nums[i]]=1
+            }
+        }
+        return false
+    }
+}
