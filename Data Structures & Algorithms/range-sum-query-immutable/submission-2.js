f06183d5188287784e178910ac0d3be1a6f33f50
ankur_singhal <62597979+AnkurSinghal05@users.noreply.github.com>
2026-08-20 22:38:12 +0530

    Add: range-sum-query-immutable - submission-2

---
+++ Data Structures & Algorithms/range-sum-query-immutable/submission-2.js
@@ -0,0 +1,24 @@
+class NumArray {
+    /**
+     * @param {number[]} nums
+     */
+    constructor(nums) {
+        this.prefix = new Array(nums.length)
+        this.prefix[0] = nums[0]
+        for(let i = 1; i<nums.length; i++){
+            this.prefix[i] = this.prefix[i-1]+nums[i]
+        }
+    }
+
+    /**
+     * @param {number} left
+     * @param {number} right
+     * @return {number}
+     */
+    sumRange(left, right) {
+        if(left==0){
+            return this.prefix[right]
+        }
+        return this.prefix[right] - this.prefix[left-1]
+    }
+}
